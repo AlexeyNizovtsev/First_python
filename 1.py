1813e154928d859af6c101c7d814dbe401c8cb1f
Alexey Nizovtsev <Niovtsev.alexey@gmail.com>
2022-10-06 20:32:35 +0300

    final commit

--- 1.py
+++ 1.py
@@ -1,1 +1,8 @@
-print('Hello')
+name= input("Введите имя студента: ").strip()
+surname= input("Введите фамилию студента: ").strip()
+year= input("Введите год рождения студента: ").strip()
+print(name+'_'+ surname+'_'+ year)
+buff=name
+name=surname
+surname=buff
+print(name+'_'+ surname+'_'+ str(int(year)+60))
